da7e9b4e6637ecfafb2aa1ed1a4f8fe035511a37
Justin Heyes-Jones <justinhj@gmail.com>
2026-06-07 21:01:00 -0700

    remove zigtree

--- zigtree/cpu_test.py
+++
@@ -1,6 +0,0 @@
-import platform
-
-print("--- Basic CPU Info ---")
-print(f"Processor: {platform.processor()}")
-print(f"Architecture: {platform.architecture()[0]}")
-print(f"Machine: {platform.machine()}")
